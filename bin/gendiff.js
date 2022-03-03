53190070e6441318a356e90956dfffb8808375ce
Igor <victororlov174@gmail.com>
2022-03-03 19:50:30 +0300

    + Help output

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+import { program } from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+program.parse(process.argv);
